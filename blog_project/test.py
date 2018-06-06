0b8d3bfedadc8209d7635088543e31df671de759
AndyQiao <qiaoconglovelife@163.com>
2018-06-06 21:52:39 +0800

    解决django与mysql时区不一致问题

--- blog_project/test.py
+++ blog_project/test.py
@@ -1,5 +1,6 @@
 import django
 import os
+from datetime import datetime
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_project.settings')
 django.setup()
 
@@ -15,10 +16,22 @@ def del_user(user_name):
     user.delete()
     #user.save()
 
+def show_archives(year, month):
+    # post_list = Article.objects.filter(
+    #     created_time__year = year,
+    #     created_time__month = month).order_by('-created_time')
+    post_list = Article.objects.filter(
+        created_time = '2018-06-06 09:04:00.000000'
+    )
+    #post_list=Article.objects.all()
+    for post in post_list:
+        print(post)
+
 if __name__ == '__main__':
     print("Starting Rango population script...")
-    show_users()
+    #show_users()
     #del_user('qiaocongAdmin')
+    show_archives('2018','5')
 
 
 
