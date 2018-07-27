76a2c14ff8fa7ba00b08120125cfe5af456e3362
AndyQiao <qiaoconglovelife@163.com>
2018-07-27 16:47:47 +0800

    屏蔽没有文章的分类

--- blog_project/blog_andy/templatetags/blog_andy_tags.py
+++ blog_project/blog_andy/templatetags/blog_andy_tags.py
@@ -1,3 +1,5 @@
+from django.db.models.aggregates import Count
+
 from django import template
 from blog_andy.models import Article, Category
 
@@ -13,6 +15,6 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('article')).filter(num_posts__gt = 0)
 
 
