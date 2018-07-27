147f501759dd44a1e9cc9e8e72df2f1d734e811a
AndyQiao <qiaoconglovelife@163.com>
2018-07-27 14:39:48 +0800

    自动摘要生成

--- blog_project/blog_andy/models.py
+++ blog_project/blog_andy/models.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import markdown
+from django.utils.html import strip_tags
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -47,6 +49,15 @@ class Article(models.Model):
         output_string = "Title:" + self.title + "_" + self.sub_title + "\n"
         output_string += ("body:"+ self.body + "\n")
         return output_string
+    
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Article, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title + '_' + self.sub_title
